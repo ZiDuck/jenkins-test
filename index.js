90d3a71822642a91bb95a3e22e6214e31c9bcebd
ZiDuck <ducducqn123.dev@gmail.com>
2024-01-31 17:14:09 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// Import essential libraries
+const express = require("express");
+const app = express();
+const path = require("path");
+const router = express.Router();
+
+app.use("/", router);
+
+app.listen(process.env.port || 3000);
+console.log("Running at Port 3000");
